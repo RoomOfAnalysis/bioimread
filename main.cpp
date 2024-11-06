7b9659b17532161e5d76583f12d58a2cf64866a8
Harold <pxduharold@126.com>
2024-11-06 13:46:47 +0800

    + call obj method

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <QApplication>
 #include <QFileInfo>
+#include <QDebug>
 
 #include <jni.h>
 
 #include <Windows.h>
-
-#include <iostream>
 
 typedef int(__stdcall* JNI_CreateJavaVMFunc)(JavaVM** pvm, JNIEnv** penv, void* args);
 
@@ -13,21 +12,25 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
+    QFileInfo info(argv[1]);
+    if (!info.exists()) qDebug() << "image file not exist at:" << info.absoluteFilePath();
+    auto img_file_path = info.absoluteFilePath().toStdString();
+
     auto java_home = qgetenv("JAVA_HOME");
-    auto jvm_dll_path = java_home + "/server/jvm.dll";
+    auto jvm_dll_path = (java_home + "/bin/server/jvm.dll").toStdString();
 
     // https://github.com/mitchdowd/jnipp/blob/master/jnipp.cpp#L1489
-    HMODULE jvmDLL = LoadLibraryA(jvm_dll_path.toStdString().c_str());
+    HMODULE jvmDLL = LoadLibraryA(jvm_dll_path.c_str());
     if (jvmDLL == 0)
     {
-        std::cerr << "failed to load jvm.dll" << std::endl;
+        qCritical() << "failed to load jvm.dll at:" << jvm_dll_path;
         exit(EXIT_FAILURE);
     }
 
     JNI_CreateJavaVMFunc Dll_JNI_CreateJavaVM = (JNI_CreateJavaVMFunc)GetProcAddress(jvmDLL, "JNI_CreateJavaVM");
     if (Dll_JNI_CreateJavaVM == 0)
     {
-        std::cerr << "failed to get jni create jvm func" << std::endl;
+        qCritical() << "failed to get jni create jvm func";
         exit(EXIT_FAILURE);
     }
 
@@ -47,35 +50,53 @@ int main(int argc, char* argv[])
 
     // Is your debugger catching an error here?  This is normal.  Just continue. The JVM
     // intentionally does this to test how the OS handles memory - reference exceptions.
+    // https://stackoverflow.com/questions/36250235/exception-0xc0000005-from-jni-createjavavm-jvm-dll/53654317#53654317
     jint rc = Dll_JNI_CreateJavaVM(&jvm, &env, &vm_args);
     delete[] options;
     if (rc != JNI_OK)
     {
-        qDebug() << "jni failed to create jvm";
+        qCritical() << "jni failed to create jvm";
         exit(EXIT_FAILURE);
     }
     jint ver = env->GetVersion();
-    std::cout << "JVM load succeeded: Version ";
-    std::cout << ((ver >> 16) & 0x0f) << "." << (ver & 0x0f) << std::endl;
+    qDebug() << "JVM load succeeded: Version" << ((ver >> 16) & 0x0f) << "." << (ver & 0x0f);
 
-    if (jclass cls = env->FindClass("loci/formats/tools/CommandLineTools"); cls == nullptr)
-        std::cerr << "can NOT find bioformats" << std::endl;
-    else if (jmethodID method_handle = env->GetStaticMethodID(cls, "printVersion", "()V"); method_handle == nullptr)
-        std::cerr << "can NOT get method" << std::endl;
+    if (jclass cls = env->FindClass("loci/formats/ImageReader"); cls == nullptr)
+        qCritical() << "can NOT find bioformats class";
     else
-        env->CallStaticObjectMethodV(cls, method_handle, {});
+    {
+        if (jmethodID constructor = env->GetMethodID(cls, "<init>", "()V"); constructor == nullptr)
+            qCritical() << "can NOT get constructor";
+        else
+        {
+            jobject instance = env->NewObject(cls, constructor);
+
+            if (jmethodID method_handle = env->GetMethodID(cls, "getFormat", "(Ljava/lang/String;)Ljava/lang/String;");
+                method_handle == nullptr)
+                qCritical() << "can NOT get method";
+            else
+            {
+                if (jstring filename = env->NewStringUTF(img_file_path.c_str()); filename == nullptr)
+                    qCritical() << "jni bad allc";
+                else
+                {
+                    jstring jstr = static_cast<jstring>(env->CallObjectMethod(instance, method_handle, filename));
+                    if (jstr == nullptr) qCritical() << "jstr null";
+                    if (env->ExceptionCheck()) env->ExceptionDescribe();
+                    char const* str = env->GetStringUTFChars(jstr, nullptr);
+                    qDebug() << str;
+                    env->ReleaseStringUTFChars(jstr, str);
+                    env->DeleteLocalRef(jstr);
+                    env->DeleteLocalRef(filename);
+                }
+            }
+
+            env->DeleteLocalRef(instance);
+        }
+    }
 
     jvm->DestroyJavaVM();
     FreeLibrary(jvmDLL);
 
-    return 0;
-
-    // QFileInfo info(argv[1]);
-    // if (info.exists())
-    // {
-    // }
-    // else
-    //     std::cout << "invalid filepath: " << argv[1] << std::endl;
-
-    // return app.exec();
+    return 0; // app.exec();
 }
