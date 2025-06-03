aebea3d288cd0a89797840a02e95f495c2055c06
Harold <pxduharold@126.com>
2025-06-03 14:07:43 +0800

    x set QImageReader allocation limit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <QApplication>
+#include <QImageReader>
 
 #include "viewer.hpp"
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+    // image allocation limit
+    // or set environment variable QT_IMAGEIO_MAXALLOC=1024 to limit to 1GB
+    // https://doc.qt.io/qt-6/qimagereader.html#setAllocationLimit
+    QImageReader::setAllocationLimit(1024); // 1GB
 
     Viewer viewer;
     viewer.showMaximized();
